d21eea242787a2ca83194d72d6e1048137d1b546
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-10-17 03:49:48 +0000

    v1.14
    Added custom slock. Thanks to https://github.com/joeldba/slock
    I did not know about the cool logo :)

---
+++ SLOCK/config.def.h
@@ -0,0 +1,32 @@
+/* user and group to drop privileges to */
+static const char *user  = "null";
+static const char *group = "null";
+
+static const char *colorname[NUMCOLS] = {
+	[BACKGROUND] =   "#0f111a",     /* after initialization */
+	[INIT] =   "#e5e9f0",     /* after initialization */
+	[INPUT] =  "#81a1c1",   /* during input */
+	[FAILED] = "#bf616a",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 40;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
