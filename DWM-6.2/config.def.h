8c67c881104bb8d3a9b65f6ce83f2b11bdca51c1
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-10-17 04:01:17 +0000

    v1.15
    Changed sizes and top bar padding.

--- DWM-6.2/config.def.h
+++ DWM-6.2/config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 8;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Liberation Mono for Powerline:size=14" };
+static const int sidepad            = 7;       /* horizontal padding of bar */
+static const char *fonts[]          = { "Liberation Mono for Powerline:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#af2626";
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
