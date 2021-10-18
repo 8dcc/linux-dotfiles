311590819288407cf9855353b529c23c3b832278
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-10-18 23:16:57 +0000

    v1.20
    Added comments and different active window border.

--- DWM-6.2/config.def.h
+++ DWM-6.2/config.def.h
@@ -1,20 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 7;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Liberation Mono for Powerline:size=15" };
-static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#af2626";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#af2626";
+static const char *fonts[]          = { "Liberation Mono for Powerline:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
+static const char col_gray1[]       = "#222222"; // Background color
+static const char col_gray2[]       = "#541414"; // Inactive window border
+static const char col_gray3[]       = "#bbbbbb"; // Font color
+static const char col_gray4[]       = "#eeeeee"; // Current tag and window FONT color
+static const char col_cyan[]        = "#af2626"; // Current tag and window color
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
