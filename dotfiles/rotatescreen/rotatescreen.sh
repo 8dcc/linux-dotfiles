e4da0aee500302c0d6915eea23aafb33e65be1c1
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-10-30 00:29:53 +0200

    v1.32

--- dotfiles/rotatescreen/rotatescreen.sh
+++ dotfiles/rotatescreen/rotatescreen.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 status="$(xrandr --query --verbose | grep "LVDS1" | cut -d ' ' -f 6)"
+deviceid="15"  # xsetwacom list
+
 
 if [[ $status == "normal" ]]; then
 	xrandr -o 1
+	xsetwacom set $deviceid Rotate ccw
 elif [[ $status == "left" ]]; then
 	xrandr -o 2
+	xsetwacom set $deviceid Rotate half
 elif [[ $status == "inverted" ]]; then
 	xrandr -o 3
+	xsetwacom set $deviceid Rotate cw
 elif [[ $status == "right" ]]; then
 	xrandr -o 0
+	xsetwacom set $deviceid Rotate none
 else
 	echo "What" > ~/.rot-errors.log
 fi
