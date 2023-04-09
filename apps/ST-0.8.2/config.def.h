88eca6e2513a72f4e40d278123554d59da5f4db7
8dcc <8dcc.git@gmail.com>
2023-04-09 04:22:01 +0200

    Add st-disable-bold-italic-fonts patch

--- apps/ST-0.8.2/config.def.h
+++ apps/ST-0.8.2/config.def.h
@@ -8,6 +8,11 @@
 static char *font = "dina:pixelsize=11:antialias=true:aut hint=true";
 static int borderpx = 2;
 
+/* disable bold, italic and roman fonts globally */
+int disablebold   = 1;
+int disableitalic = 1;
+int disableroman  = 1;
+
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
