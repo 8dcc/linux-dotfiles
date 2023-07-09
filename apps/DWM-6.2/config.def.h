ee888feeda07c04755fb9c69ccaa786752f9be1e
8dcc <8dcc.git@gmail.com>
2023-07-09 13:23:30 +0200

    Add --hidecursor to maim

--- apps/DWM-6.2/config.def.h
+++ apps/DWM-6.2/config.def.h
@@ -109,8 +109,8 @@ static Key keys[] = {
     { MODKEY,           XK_k,      focusstack,     {.i = -1 } },
     { MODKEY,           XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY,           XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY,           XK_F12,    spawn,          SHCMD("maim -s | tee ~/ss-$(date +%s).png | xclip -selection clipboard -t image/png") },
-    { MODKEY|ShiftMask, XK_F12,    spawn,          SHCMD("maim | tee ~/ss-$(date +%s).png | xclip -selection clipboard -t image/png") },
+    { MODKEY,           XK_F12,    spawn,          SHCMD("maim --hidecursor -s | tee ~/ss-$(date +%s).png | xclip -selection clipboard -t image/png") },
+    { MODKEY|ShiftMask, XK_F12,    spawn,          SHCMD("maim --hidecursor | tee ~/ss-$(date +%s).png | xclip -selection clipboard -t image/png") },
     { MODKEY|ShiftMask, XK_l,      spawn,          SHCMD("slock") },
     { MODKEY,           XK_e,      spawn,          SHCMD("emacsclient -c") },
 };
