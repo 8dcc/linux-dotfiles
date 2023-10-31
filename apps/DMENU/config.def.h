430f412d41811b4eb8373304a970d7c9ea6874cb
8dcc <8dcc.git@gmail.com>
2023-10-31 22:30:51 +0100

    Use pixelsize for DMENU, add cozette as fallback

--- apps/DMENU/config.def.h
+++ apps/DMENU/config.def.h
@@ -1,11 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char* fonts[] = { "dina:size=8" };
-static const char* prompt = NULL; /* -p  option; prompt to the left of input field */
+static const char* fonts[] = {
+    "dina:pixelsize=10",
+    "Cozette:pixelsize=11",
+};
+
+/* -p  option; prompt to the left of input field */
+static const char* prompt = NULL;
 
 static const char* colors[SchemeLast][2] = {
     /*     fg         bg       */
