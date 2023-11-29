0b172e9bf30b3617a93a167f7eefcaa44ba69bb2
8dcc <8dcc.git@gmail.com>
2023-11-29 17:20:53 +0100

    Remove Gimp rule, add ida.exe rule

--- apps/DWM-6.2/config.def.h
+++ apps/DWM-6.2/config.def.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+    { "ida.exe",  NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
