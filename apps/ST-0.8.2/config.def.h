599033297a33e77651133b6dc00c139ce7a1a7c2
8dcc <8dcc.git@gmail.com>
2023-12-03 15:47:49 +0000

    Increase font size for ST

--- apps/ST-0.8.2/config.def.h
+++ apps/ST-0.8.2/config.def.h
@@ -9,8 +9,8 @@
  *   - "creep2:pixelsize=11"
  *   - "Cozette:pixelsize=11"
  */
-static char *font = "Dina:pixelsize=8";
-static char *font2 = "Cozette:pixelsize=8";
+static char *font = "Dina:pixelsize=11";
+static char *font2 = "Cozette:pixelsize=11";
 
 static int borderpx = 2;
 
