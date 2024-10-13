fb9769419d7d469e56667c93aba341f08a7a0070
8dcc <8dcc.git@gmail.com>
2024-10-13 16:28:01 +0200

    Comment unused ST keybinds

--- apps/ST-0.8.2/config.def.h
+++ apps/ST-0.8.2/config.def.h
@@ -226,19 +226,23 @@ MouseKey mkeys[] = {
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
+#if 0
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+#endif
 	{ ControlMask,          XK_plus,        zoom,           {.f = +1} },  /* Increase zoom with Ctrl and + */
 	{ ControlMask,          XK_minus,       zoom,           {.f = -1} },  /* Decrease zoom with Ctrl and - */
 	{ ControlMask,          XK_Home,        zoomreset,      {.f =  0} },  /* Reset zoom level with Ctrl and Home */
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+#if 0
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+#endif
 	{ TERMMOD,              XK_W,           togglewide,     {.i =  0} },
 };
 
