f64457768742c84615d884c062e902c902b01bd5
8dcc <8dcc.git@gmail.com>
2024-11-22 18:08:16 +0100

    st: Update config.mk to be more system-agnostic

--- apps/ST-0.8.2/config.mk
+++ apps/ST-0.8.2/config.mk
@@ -7,18 +7,10 @@ VERSION = 0.8.2
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
 PKG_CONFIG = pkg-config
 
-# includes and libs
-INCS = -I$(X11INC) \
-       `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
-       `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
+INCS = $(shell pkg-config --cflags x11 fontconfig freetype2)
+LIBS = -lm -lrt $(shell pkg-config --libs x11 freetype2 xft fontconfig)
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
@@ -32,4 +24,4 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 #       `pkg-config --libs freetype2`
 
 # compiler and linker
-# CC = c99
+CC = gcc
