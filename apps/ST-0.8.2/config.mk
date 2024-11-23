50d8d779ef01e03a73c7361b4220e13aebbb92de
8dcc <8dcc.git@gmail.com>
2024-11-23 11:45:42 +0100

    st: Simplify config.mk

--- apps/ST-0.8.2/config.mk
+++ apps/ST-0.8.2/config.mk
@@ -1,21 +1,18 @@
 # st version
 VERSION = 0.8.2
 
-# Customize below to fit your system
-
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX    ?= /usr/local
+MANPREFIX ?= $(PREFIX)/share/man
 
-PKG_CONFIG = pkg-config
-
+# includes and libs
 INCS = $(shell pkg-config --cflags x11 fontconfig freetype2)
 LIBS = -lm -lrt $(shell pkg-config --libs x11 freetype2 xft fontconfig)
 
 # flags
-STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
-STLDFLAGS = $(LIBS) $(LDFLAGS)
+STCPPFLAGS ?= -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
+STCFLAGS   ?= $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STLDFLAGS  ?= $(LIBS) $(LDFLAGS)
 
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
