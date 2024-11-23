c38c509dc159a2eae3102e0d3e71e74516ba61ea
8dcc <8dcc.git@gmail.com>
2024-11-23 15:23:37 +0100

    slock: Update config.mk to be more system-agnostic

--- apps/SLOCK/config.mk
+++ apps/SLOCK/config.mk
@@ -1,40 +1,25 @@
 # slock version
 VERSION = 1.4
 
-# Customize below to fit your system
+# user for slock
+SLOCKUSER ?= slock
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
-
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
-
-# Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
-
-# freetype
-FREETYPELIBS = -lXft
-FREETYPEINC = /usr/include/freetype2
+PREFIX    ?= /usr/local
+MANPREFIX ?= ${PREFIX}/share/man
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC} -I${FREETYPEINC}
 LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXext -lXrandr
 
-# flags
-CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H ${XINERAMAFLAGS}
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -s ${LIBS}
-COMPATSRC = explicit_bzero.c
+INCS = $(shell pkg-config --cflags x11 fontconfig freetype2)
+LIBS = -lc -lcrypt $(shell pkg-config --libs x11 fontconfig freetype2 xinerama xext xrandr)
 
-# On OpenBSD and Darwin remove -lcrypt from LIBS
-#LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lXext -lXrandr
-# On *BSD remove -DHAVE_SHADOW_H from CPPFLAGS
-# On NetBSD add -D_NETBSD_SOURCE to CPPFLAGS
-#CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE -D_NETBSD_SOURCE
-# On OpenBSD set COMPATSRC to empty
-#COMPATSRC =
+# flags
+CPPFLAGS  ?= -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H -DXINERAMA
+CFLAGS    ?= -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS   ?= -s ${LIBS}
+COMPATSRC ?= explicit_bzero.c
 
 # compiler and linker
-CC = cc
+CC = gcc
