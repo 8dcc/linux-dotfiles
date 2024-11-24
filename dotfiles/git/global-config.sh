3a9c9f0d0b06675891125922ab8066dc6a7ef88e
8dcc <8dcc.git@gmail.com>
2024-11-24 01:34:19 +0100

    git: Add message to global-config.sh

--- dotfiles/git/global-config.sh
+++ dotfiles/git/global-config.sh
@@ -14,3 +14,5 @@ set_git_config "credential.helpers" "store"
 set_git_config "commit.gpgsign"     "true"
 set_git_config "pull.rebase"        "false"
 set_git_config "init.defaultbranch" "main"
+
+echo "All done."
