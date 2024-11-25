a498a31f55360e569b2d9a0d83d43e87a3f4a365
8dcc <8dcc.git@gmail.com>
2024-11-25 14:31:23 +0100

    startx.sh: Check the current TTY before calling xinit
    We could change the argument we pass to 'xinit' depending on the value of
    $(tty), but I rather only start Xorg on TTY1.

--- scripts/startup/startx.sh
+++ scripts/startup/startx.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 PREFIX="$HOME/.guix-profile"
+DEV="/dev/tty1"
+
+if [ "$(tty)" != "$DEV" ]; then
+    echo "$(basename "$0"): Current TTY is not whitelisted, ignoring." 1>&2
+    exit 1
+fi
 
 # NOTE: Intended to run on TTY1 (vt1)
 $PREFIX/bin/xinit -- $PREFIX/bin/Xorg :0 vt1 -keeptty \
