aafcf7f855cea810092741171ed2a82fd1182fd2
8dcc <8dcc.git@gmail.com>
2024-12-11 13:12:22 +0100

    git: Add 'commit.verbose' to 'global-config.sh'

--- dotfiles/git/global-config.sh
+++ dotfiles/git/global-config.sh
@@ -12,6 +12,7 @@ set_git_config "user.signingkey"    "228C104965317F9A"
 set_git_config "core.editor"        "nvim"
 set_git_config "credential.helpers" "store"
 set_git_config "commit.gpgsign"     "true"
+set_git_config "commit.verbose"     "true"
 set_git_config "pull.rebase"        "false"
 set_git_config "init.defaultbranch" "main"
 
