b4e59d47fde7eab707168350d5a062670158cf8b
8dcc <8dcc.git@gmail.com>
2026-03-26 18:08:06 +0100

    Set 'core.hooksPath' to '~/.config/git/hooks' if it exists

--- dotfiles/git/global-config.sh
+++ dotfiles/git/global-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+HOOKS_DIR="${HOME}/.config/git/hooks"
+
 set_git_config() {
     git config --global "$1" "$2"
 }
@@ -12,8 +14,14 @@ set_git_config "user.signingkey"    "228C104965317F9A"
 set_git_config "core.editor"        "nvim"
 set_git_config "credential.helper"  "store"
 set_git_config "commit.gpgsign"     "true"
-set_git_config "commit.verbose"     "true"
+set_git_config "commit.verbose"     "false"    # Perhaps change in some systems
 set_git_config "pull.rebase"        "false"
 set_git_config "init.defaultbranch" "main"
+
+if [ -d "$HOOKS_DIR" ]; then
+    set_git_config "core.hooksPath" "$HOOKS_DIR"
+else
+    echo "warning: Directory '${HOOKS_DIR}' does not exist. Not assining." 1>&2
+fi
 
 echo "All done."
